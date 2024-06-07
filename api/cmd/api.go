2492e3be4a5b34516b9e27ade3c75577c25c13e0
krispekla <peklarick@gmail.com>
2024-06-07 21:18:40 +0200

    Add supertokens init

--- api/cmd/api.go
+++ api/cmd/api.go
@@ -9,6 +9,12 @@ import (
 	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
+
+	"github.com/supertokens/supertokens-golang/recipe/emailpassword"
+	"github.com/supertokens/supertokens-golang/recipe/session"
+	"github.com/supertokens/supertokens-golang/recipe/thirdparty"
+	"github.com/supertokens/supertokens-golang/recipe/thirdparty/tpmodels"
+	"github.com/supertokens/supertokens-golang/supertokens"
 )
 
 var db *sql.DB
@@ -25,6 +31,33 @@ func main() {
 		fmt.Println("Failed to open database:", err)
 		return
 	}
+
+	apiBasePath := "/auth"
+	websiteBasePath := "/auth"
+	err = supertokens.Init(supertokens.TypeInput{
+		Supertokens: &supertokens.ConnectionInfo{
+			// https://try.supertokens.com is for demo purposes. Replace this with the address of your core instance (sign up on supertokens.com), or self host a core.
+			ConnectionURI: "https://try.supertokens.com",
+			// APIKey: <API_KEY(if configured)>,
+		},
+		AppInfo: supertokens.AppInfo{
+			AppName:         "test",
+			APIDomain:       "http://localhost:3003",
+			WebsiteDomain:   "http://localhost:8084",
+			APIBasePath:     &apiBasePath,
+			WebsiteBasePath: &websiteBasePath,
+		},
+		RecipeList: []supertokens.Recipe{
+			thirdparty.Init(&tpmodels.TypeInput{ /*TODO: See next step*/ }),
+			emailpassword.Init(nil),
+			session.Init(nil), // initializes session features
+		},
+	})
+
+	if err != nil {
+		panic(err.Error())
+	}
+
 	r := chi.NewRouter()
 
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
