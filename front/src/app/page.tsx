ed7aa978e012efa86ff04f2271a0adb2bf03b7c3
krispekla <peklarick@gmail.com>
2024-06-08 23:31:18 +0200

    Move session information into main page

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,8 +1,32 @@
+"use client";
 import Image from "next/image";
+import { useSessionContext } from "supertokens-auth-react/recipe/session";
 
 export default function Home() {
+  const session = useSessionContext();
+
+  const sessionContent = session.loading ? (
+    <div>Loading...</div>
+  ) : session.doesSessionExist ? (
+    <div>
+      <div>
+        <p>
+          Client side component got userId: {session.userId}
+          <br />
+          {JSON.stringify(session, null, 4)}
+        </p>
+      </div>
+    </div>
+  ) : (
+    <div>Session does not exist</div>
+  );
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="flex flex-col">
+        <h2 className="font-bold text-lg">Session:</h2>
+        {sessionContent}
+      </div>
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
