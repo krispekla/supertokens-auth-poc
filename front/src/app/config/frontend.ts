7b94bdd4cd5efe27a698026c23d6a9b09cd29544
krispekla <peklarick@gmail.com>
2024-06-14 13:29:54 +0200

    Customizing auth screen css

--- front/src/app/config/frontend.ts
+++ front/src/app/config/frontend.ts
@@ -29,6 +29,40 @@ export const frontendConfig = (): SuperTokensConfig => {
             ThirdPartyReact.Apple.init(),
           ],
         },
+        style: `
+            [data-supertokens~="superTokensBranding"] {
+              display: none !important;
+          }
+
+          [data-supertokens~="formRow"]:last-child {
+              [data-supertokens~="button"] {
+                  height: 41px;
+              }
+          }
+          
+          [data-supertokens~="input"], [data-supertokens~="inputWrapper"] {
+            height: 38px;
+          }
+
+          [data-supertokens~="button"][data-supertokens~="providerButton"] {
+          min-height: 38px;
+          }
+            
+           [data-supertokens~=container] {
+            --palette-background: 51, 51, 51 !important;
+            --palette-inputBackground: 41, 41, 41 !important;
+            --palette-inputBorder: 41, 41, 41 !important;
+            --palette-textTitle: 255, 255, 255 !important;
+            --palette-textLabel: 255, 255, 255 !important;
+            --palette-textPrimary: 255, 255, 255 !important;
+            --palette-primary: 51, 112, 255 !important;
+            --palette-primaryBorder: 4, 51, 161 !important;
+            --palette-error: 173, 46, 46 !important;
+            --palette-textInput: 169, 169, 169 !important;
+            --palette-textLink: 169, 169, 169 !important;
+            --font-size-1: 16px !important;
+        }
+        `,
       }),
       EmailPasswordReact.init(),
       SessionReact.init(),
