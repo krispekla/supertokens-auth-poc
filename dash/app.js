f66bc6c36e250d06f4d98aa64d971a7fd4b5f5b3
krispekla <peklarick@gmail.com>
2024-07-31 16:14:50 +0200

    Fix missing session and add third party

--- dash/app.js
+++ dash/app.js
@@ -8,6 +8,8 @@ import morgan from "morgan";
 import supertokens from "supertokens-node";
 import { middleware } from "supertokens-node/framework/express";
 import Dashboard from "supertokens-node/recipe/dashboard";
+import Session from "supertokens-node/recipe/session";
+import ThirdParty from "supertokens-node/recipe/thirdparty";
 
 dotenv.config();
 
@@ -31,7 +33,44 @@ supertokens.init({
     websiteBasePath: "/auth",
   },
   recipeList: [
-    Dashboard.init()
+    Session.init(),
+    ThirdParty.init({
+      signInAndUpFeature: {
+        // We have provided you with development keys which you can use for testing.
+        // IMPORTANT: Please replace them with your own OAuth keys for production use.
+        providers: [
+          {
+            config: {
+              thirdPartyId: "google",
+              clients: [
+                {
+                  clientId:
+                    "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
+                  clientSecret: "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",
+                },
+              ],
+            },
+          },
+          {
+            config: {
+              thirdPartyId: "apple",
+              clients: [
+                {
+                  clientId: "4398792-io.supertokens.example.service",
+                  additionalConfig: {
+                    keyId: "7M48Y4RYDL",
+                    privateKey:
+                      "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
+                    teamId: "YWQCXGJRJL",
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    }),
+    Dashboard.init(),
   ],
 });
 
