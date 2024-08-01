d3e1e5f2f464a7c5257b7a30faee422c1c61af56
krispekla <peklarick@gmail.com>
2024-08-01 23:29:00 +0200

    Implement custom email delivery

--- api/cmd/api.go
+++ api/cmd/api.go
@@ -14,7 +14,10 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/mailgun/mailgun-go/v4"
 
+	"github.com/supertokens/supertokens-golang/ingredients/emaildelivery"
 	"github.com/supertokens/supertokens-golang/recipe/emailpassword"
+	"github.com/supertokens/supertokens-golang/recipe/emailverification"
+	"github.com/supertokens/supertokens-golang/recipe/emailverification/evmodels"
 	"github.com/supertokens/supertokens-golang/recipe/session"
 	"github.com/supertokens/supertokens-golang/recipe/session/sessmodels"
 	"github.com/supertokens/supertokens-golang/recipe/thirdparty"
@@ -55,6 +58,25 @@ func main() {
 		RecipeList: []supertokens.Recipe{
 			thirdparty.Init(&tpmodels.TypeInput{ /*TODO: See next step*/ }),
 			emailpassword.Init(nil),
+			emailverification.Init(evmodels.TypeInput{
+				Mode: evmodels.ModeRequired,
+				EmailDelivery: &emaildelivery.TypeInput{
+					Override: func(originalImplementation emaildelivery.EmailDeliveryInterface) emaildelivery.EmailDeliveryInterface {
+						originalSendEmail := *originalImplementation.SendEmail
+
+						(*originalImplementation.SendEmail) = func(input emaildelivery.EmailType, userContext supertokens.UserContext) error {
+							// TODO: create and email verification email
+							(*originalImplementation.SendEmail) = CustomSendEmail
+
+							// Or use the original implementation which calls the default service,
+							// or a service that you may have specified in the EmailDelivery object.
+							return originalSendEmail(input, userContext)
+						}
+
+						return originalImplementation
+					},
+				},
+			}),
 			session.Init(nil), // initializes session features
 			userroles.Init(nil),
 		},
@@ -108,6 +130,7 @@ func main() {
 	http.ListenAndServe(":3003", r)
 }
 
+// TODO: Remove and refactor this with func below for sending email
 func SendSimpleMessage(ctx context.Context, domain, apiKey, msg, emailAdd, sender string) (string, error) {
 	mg := mailgun.NewMailgun(domain, apiKey)
 	m := mg.NewMessage(
@@ -118,4 +141,21 @@ func SendSimpleMessage(ctx context.Context, domain, apiKey, msg, emailAdd, sende
 	)
 	_, id, err := mg.Send(ctx, m)
 	return id, err
+}
+
+// CustomSendEmail implements the SendEmail method of emaildelivery.EmailDeliveryInterface
+func CustomSendEmail(input emaildelivery.EmailType, userContext supertokens.UserContext) error {
+	// TODO: create and email verification email
+	_, err := SendSimpleMessage(
+		context.Background(),
+		os.Getenv("MAILGUN_DOMAIN"),
+		os.Getenv("MAILGUN_API_KEY"),
+		fmt.Sprintf("Verify email: %s", input.EmailVerification.EmailVerifyLink),
+		input.EmailVerification.User.Email,
+		os.Getenv("MAILGUN_SENDER_EMAIL"),
+	)
+	if err != nil {
+		return err
+	}
+	return nil
 }
