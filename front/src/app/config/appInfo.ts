c914b5b774cc1f0c5ea09fabca69b6cae04c4499
krispekla <peklarick@gmail.com>
2024-08-04 12:04:02 +0200

    Make api public so ti can be used in spa mode

--- front/src/app/config/appInfo.ts
+++ front/src/app/config/appInfo.ts
@@ -1,6 +1,6 @@
 export const appInfo = {
-  appName: process.env.APP_NAME || "test",
-  apiDomain: process.env.API_DOMAIN || "http://localhost:3003",
+  appName: process.env.NEXT_PUBLIC_APP_NAME || "test",
+  apiDomain: process.env.NEXT_PUBLIC_API_DOMAIN || "http://localhost:3003",
   websiteDomain: process.env.WEBSITE_DOMAIN || "http://localhost:8084",
   apiBasePath: process.env.API_BASE_PATH || "/auth",
   websiteBasePath: process.env.WEBSITE_BASE_PATH || "/auth",
